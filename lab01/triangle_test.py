ebecc7763d728adfc0690f12ab7645153daa7f7e
m3tro1d <qazwf734@gmail.com>
2022-09-13 19:20:30 +0300

    lab01: improve testing script, add more cases

--- lab01/triangle_test.py
+++ lab01/triangle_test.py
@@ -4,6 +4,9 @@ import subprocess
 
 EXIT_FAILURE = 1
 USAGE = 'usage: python triangle_test.py triangle.py test_cases.txt result.txt'
+STRIP_CHARS = ' \n\r'
+TEST_DELIMITER = ':'
+COMMENT_INDICATOR = '#'
 
 SUCCESS = 'success'
 ERROR = 'error'
@@ -11,8 +14,8 @@ ERROR = 'error'
 
 class TestCase:
     def __init__(self, args, expected_output):
-        self._args = args
-        self._expected_output = expected_output
+        self._args = args.split(' ')
+        self._expected_output = expected_output.strip(STRIP_CHARS)
 
     def get_args(self):
         return self._args
@@ -26,10 +29,37 @@ def parse_test_file(filename):
 
     with open(filename, mode='r', encoding='utf-8') as f:
         for line in f.readlines():
-            elements = line.strip(' \n\r').split(' ')
-            result.append(TestCase(elements[:3], elements[-1]))
+            line = line.strip(STRIP_CHARS)
+
+            if len(line) == 0 or line.startswith(COMMENT_INDICATOR):
+                continue
+
+            args, output = line.split(TEST_DELIMITER)
+            result.append(TestCase(args, output))
 
     return result
+
+
+def run_tests(test_cases, script_name, result_file):
+    total_cases = len(test_cases)
+    passed_cases = 0
+
+    with open(result_file, mode='w', encoding='utf-8') as f:
+        for test_case in test_cases:
+            result = subprocess.run(
+                ['python', script_name, *test_case.get_args()],
+                stdout=subprocess.PIPE
+            )
+            output = result.stdout.decode('1251').strip(STRIP_CHARS)
+
+            if output == test_case.get_expected_output():
+                print(SUCCESS, file=f)
+                passed_cases += 1
+            else:
+                print(ERROR, file=f)
+
+        result_str = '\npassed {}/{} - {:.2%}'.format(passed_cases, total_cases, passed_cases / total_cases)
+        print(result_str, file=f)
 
 
 def main(args):
@@ -41,19 +71,8 @@ def main(args):
     test_file = args[1]
     result_file = args[2]
 
-    with open(result_file, mode='w', encoding='utf-8') as f:
-        test_cases = parse_test_file(test_file)
-        for test_case in test_cases:
-            result = subprocess.run(
-                ['python', script_name, *test_case.get_args()],
-                stdout=subprocess.PIPE
-            )
-            output = result.stdout.decode('1251').strip(' \n\r')
-
-            if output == test_case.get_expected_output():
-                print(SUCCESS, file=f)
-            else:
-                print(ERROR, file=f)
+    test_cases = parse_test_file(test_file)
+    run_tests(test_cases, script_name, result_file)
 
 
 if __name__ == '__main__':
